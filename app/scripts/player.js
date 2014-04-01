882abe180f676e5df427abbfd102900b51e635f6
Ivar <ivaroddsson@gmail.com>
2014-04-01 13:05:10 +0000

    Now game does not end when bird goes to high

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -8,7 +8,7 @@ window.Player = (function() {
 	//var SPEED = 20; // * 10 pixels per second
 	var GRAVITY = 170;
 	var JUMP_VELOCITY = 50;
-	var WIDTH = 5;
+	//var WIDTH = 5;
 	var HEIGHT = 5;
 	var INITIAL_POSITION_X = 15;
 	var INITIAL_POSITION_Y = 25;
@@ -46,10 +46,7 @@ window.Player = (function() {
 	};
 
 	Player.prototype.checkCollisionWithBounds = function() {
-		if (this.pos.x < 0 ||
-			this.pos.x + WIDTH > this.game.WORLD_WIDTH ||
-			this.pos.y < 0 ||
-			this.pos.y + HEIGHT > this.game.WORLD_HEIGHT) {
+		if (this.pos.y + HEIGHT > this.game.WORLD_HEIGHT) {
 			return this.game.gameover();
 		}
 	};
